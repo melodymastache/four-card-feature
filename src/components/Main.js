2c4ebd2b85e26697367783ed0e16b5f944c5eef3
Melody Mastache <melodymastache@gmail.com>
2020-05-05 15:24:37 -0400

    whoops

--- src/components/Main.js
+++ src/components/Main.js
@@ -21,10 +21,10 @@ class Main extends React.Component {
                 <div className="cardGrid" id="builderCard">
                     <Card imgSrc={builderIcon} title="Team Builder" text="Scans our talent network to create the optimal team for your project" />
                 </div>
-                <div className="cardGrid" id="calculatorCard">
+                <div className="cardGrid outerCards" id="calculatorCard">
                     <Card imgSrc={calculatorIcon} title="Calculator" text="Uses data from past projects to provide better delivery estimates" />
                 </div>
-                <div className="cardGrid outerCards" id="karmaCard">
+                <div className="cardGrid" id="karmaCard">
                     <Card imgSrc={karmaIcon} title="Karma" text="Regularly evaluates our talent to ensure quality" />
                 </div>
             </div>
